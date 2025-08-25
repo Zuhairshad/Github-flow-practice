c1e9843d6772ebf76c9e2aa7f67211702dd743cd
Zuhair Shad <123764183+Zuhairshad@users.noreply.github.com>
2025-08-25 23:26:57 +0500

    add a button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import Newfeature from './Components/Newfeature'
 
 function App() {
   return (
     <div>
       <h1>Hello, React!</h1>
       <p>This is a basic React app with Vite.</p>
+      <NewFeature/>
     </div>
+    
   )
 }
 
